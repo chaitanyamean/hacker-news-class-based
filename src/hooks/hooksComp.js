bc6517a5a1cc86e84e3361acdebb5499ddd0570f
chaitanyamean <chaitanyamean@gmail.com>
2020-10-27 21:42:55 +0530

    hooks component

---
+++ src/hooks/hooksComp.js
@@ -0,0 +1,86 @@
+
+
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
+
+const HooksComponent = () => {
+
+    const [count, setCount] = useState(0)
+    const [apiStr, setApiStr] = useState('topstories')
+    const [stories, setStories] = useState([])
+
+
+
+
+
+    // to call some thing after ui rendering - api, subscribe, 
+
+        useEffect(() => {
+            const fetchApi = () => {
+                fetchStoriesApi(apiStr)
+            }
+            fetchApi()
+
+        }, [apiStr])
+
+
+        // useEffect(() => {
+        //     const fetchApi = () => {
+        //         fetchStoriesApi(apiStr)
+        //     }
+        //     fetchApi()
+
+        // }, [count])
+
+
+
+
+    const fetchStoriesApi = (str) => {
+        // we need to call api
+
+        console.log('STORIES', str);
+
+        // https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
+
+        // 1. we will call top stories api
+
+        let requests = []
+
+            axios.get(`https://hacker-news.firebaseio.com/v0/${str}.json`)
+                 .then(res => {
+                     console.log(res)
+                     res.data.map((item, idx) => {
+                            if(idx < 20) {
+                                // // reducing upto 20 
+                                requests.push(
+                                    axios.get(`https://hacker-news.firebaseio.com/v0/item/${item}.json`)
+                                )
+                            }
+                     });
+
+                     // Promise 
+                     Promise.all(requests).then(data => {
+                         console.log(data)
+                            // this.setState({stories: data})
+                            setStories(data)
+                     })
+
+                 })
+
+                 console.log(stories)
+
+    } 
+
+
+
+    return(
+        <div>
+            This is func component {count}
+            <button onClick={() =>setApiStr('askstories')}>Ask stories</button>
+        </div>
+    )
+
+}
+
+export default HooksComponent
